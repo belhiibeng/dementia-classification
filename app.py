4607eef9e35bafc682a05325f7a2dd693075b0a4
belhiibeng <belhi.romdona@gmail.com>
2022-06-13 00:23:02 +0700

    app update

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ import joblib
 st.header('Dementia Classification App')
 
 # Dropdown input
-gender = st.selectbox('Select Gender', ('Male', 'Female'))
+gender = st.selectbox('Select Gender of The Subject', ('Male', 'Female'))
 
 # Number input 1
-age = st.number_input('Enter Age', 0, 1000)
+age = st.number_input('Enter Age of The Subject', 10, 100)
 
 # Slider input 1
-educ = st.number_input('Enter Education Level', 1, 5)
+educ = st.number_input('Enter Educational Level of The Subject', 1, 5)
 
 # Slider input 2
-ses = st.number_input('Enter Socio-Economic Status Level', 1, 5)
+ses = st.number_input('Enter Socio-Economic Status Level of The Subject', 1, 5)
 
 # Slider input 3
-mmse = st.number_input('Enter Mini-Mental State Examination Score', 0, 30)
+mmse = st.number_input('Enter Mini-Mental State Examination Score of The Subject', 0, 30)
 
 # Input bar 5
-etiv = st.number_input('Enter estimated Total Intercranial Volume of the brain in cubic milimeters', 0.0 , 2000.0)
+etiv = st.number_input('Enter Estimated Total Intercranial Volume of The Brain in Cubic Milimeters of The Subject', 1000.0 , 2000.0)
 
 # Input bar 6
-nwbv = st.number_input('Enter normalized Whole Brain Volume in miligrams', 0.0, 1.0)
+nwbv = st.number_input('Enter normalized Whole Brain Volume in Miligrams of The Subject', 0.5, 1.0)
 
 # If button is pressed
 if st.button("Submit"):
@@ -42,10 +42,10 @@ if st.button("Submit"):
     
     # Output prediction
     if prediction == '0.0':
-        st.text('The subject is cognitively normal')
+        st.text('The Subject Is Cognitively Normal')
     elif prediction == '0.5':
-        st.text('The subject has very mild dementia')
+        st.text('The Subject Has Very Mild Dementia')
     elif prediction == '1.0':
-        st.text('The subject has mild dementia')
+        st.text('The Subject Has Mild Dementia')
     elif prediction == '2.0':
         st.text('The subject has moderate dementia')
