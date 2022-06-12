cc07f09a8182c26b483ebb521403614a7d30e1d7
belhiibeng <belhi.romdona@gmail.com>
2022-06-13 00:12:00 +0700

    app update

--- app.py
+++ app.py
@@ -3,28 +3,28 @@ import pandas as pd
 import joblib
 
 # Title
-st.header("Dementia Classification App")
+st.header('Dementia Classification App')
 
 # Dropdown input
-gender = st.selectbox("Select Gender", ("Male", "Female"))
+gender = st.selectbox('Select Gender', ('Male', 'Female'))
 
-# Input bar 1
-age = st.number_input("Enter Age")
+# Number input 1
+age = st.number_input('Enter Age', 0, 1000)
 
-# Input bar 2
-educ = st.number_input("Enter Educ")
+# Slider input 1
+educ = st.number_input('Enter Education Level', 1, 5)
 
-# Input bar 3
-ses = st.number_input("Enter SES")
+# Slider input 2
+ses = st.number_input('Enter Socio-Economic Status Level', 1, 5)
 
-# Input bar 4
-mmse = st.number_input("Enter MMSE")
+# Slider input 3
+mmse = st.number_input('Enter Mini-Mental State Examination Score', 0, 30)
 
 # Input bar 5
-etiv = st.number_input("Enter eTIV")
+etiv = st.number_input('Enter estimated Total Intercranial Volume of the brain in cubic milimeters', 0.0 , 2000.0)
 
 # Input bar 6
-nwbv = st.number_input("Enter nWBV")
+nwbv = st.number_input('Enter normalized Whole Brain Volume in miligrams', 0.0, 1.0)
 
 # If button is pressed
 if st.button("Submit"):
@@ -35,10 +35,17 @@ if st.button("Submit"):
     # Store inputs into dataframe
     X = pd.DataFrame([[gender, age, educ, ses, mmse, etiv, nwbv]], 
                      columns = ['M/F', 'Age', 'Educ', 'SES', 'MMSE', 'eTIV', 'nWBV'])
-    X = X.replace(["Male", "Female"], ['M', 'F'])
+    X = X.replace(['Male', 'Female'], ['M', 'F'])
     
     # Get prediction
     prediction = clf.predict(X)[0]
     
     # Output prediction
-    st.text(f"Your CDR is  {prediction}")
+    if prediction == '0.0':
+        st.text('The subject is cognitively normal')
+    elif prediction == '0.5':
+        st.text('The subject has very mild dementia')
+    elif prediction == '1.0':
+        st.text('The subject has mild dementia')
+    elif prediction == '2.0':
+        st.text('The subject has moderate dementia')
